e92ddda1dec91570a183b829d1897e66b86836f9
zesciati <zakyabiyyu2003@gmail.com>
2025-12-27 17:50:40 +0700

    feat: interaktivitas dan local storage

--- script.js
+++ script.js
@@ -11,18 +11,42 @@ const toThankYou = document.querySelector(".button-proceed");
 const backToOrder = document.querySelector(".button-back");
 
 toThankYou.addEventListener("click", toThank);
+
 backToOrder.addEventListener("click", backTo);
 
 
 
-function backTo(){
+function backTo() {
   thankYou.classList.add("hidden");
   order.classList.remove("hidden");
+
+  localStorage.setItem("pageState", "order")
 }
 
 
-function toThank(){
-  
+function toThank() {
   order.classList.add("hidden");
   thankYou.classList.remove("hidden");
+
+  localStorage.setItem("pageState", "thank")
 };
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  thankYou.classList.add("hidden");
+  order.classList.remove("hidden");
+
+
+  const state = localStorage.getItem("pageState");
+
+  if (state === "order") {
+    thankYou.classList.add("hidden");
+    order.classList.remove("hidden");
+  } else {
+    order.classList.add("hidden");
+    thankYou.classList.remove("hidden");
+
+  }
+
+
+});
