4f840b1b578c247893b11992c3e7f9dca1c192a6
zesciati <zakyabiyyu2003@gmail.com>
2025-12-27 11:50:29 +0700

    feat: menambahkan interaktifitas js

--- script.js
+++ script.js
@@ -2,7 +2,27 @@
 - panggil konten nya 
 - ketika button di pencet akan ada konten menggantikan order summary
 - akan muncul konten thank you
+
 */
 
-const thank = document.getElementsByClassName("thank-you");
+const thankYou = document.querySelector(".thank-you");
+const order = document.querySelector(".order");
+const toThankYou = document.querySelector(".button-proceed");
+const backToOrder = document.querySelector(".button-back");
 
+toThankYou.addEventListener("click", toThank);
+backToOrder.addEventListener("click", backTo);
+
+
+
+function backTo(){
+  thankYou.classList.add("hidden");
+  order.classList.remove("hidden");
+}
+
+
+function toThank(){
+  
+  order.classList.add("hidden");
+  thankYou.classList.remove("hidden");
+};
